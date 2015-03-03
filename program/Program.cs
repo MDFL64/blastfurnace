41bf6f57029082767209ddfd38b1224eaf9a21c8
Adam Coggeshall <dameerkat@gmail.com>
2015-03-02 20:27:59 -0700

    C# Setup

---
+++ program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using SteamKit2.GC;
+using MsgSys = SteamKit2.GC.Internal;
+using MsgTF = SteamKit2.GC.TF2.Internal;
+
+namespace program {
+	class Program {
+		static void Main(string[] args) {
+
+		}
+	}
+}
